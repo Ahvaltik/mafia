1f1f5aafb23c836c7d101407d3d687db45a65128
wilczogon <wilczogon@gmail.com>
2016-01-20 20:28:18 +0100

    Not checked if correct - probably it is

---
+++ TestDifficultyManager.py
@@ -0,0 +1,37 @@
+import agent
+from NSAModule import NSAagent
+
+class TestDifficultyManager:
+	def __init__(self, system, nsa_elements, separability = "medium", gangToCivilRatio = "medium", numberOfCivilians = "medium"):
+		self.system = system
+		self.nsa_elements = nsa_elements
+		self.separability = separability
+		self.gangToCivilRatio = gangToCivilRatio
+		self.numberOfCivilians = numberOfCivilians
+		
+	def generateName(self):
+		pass
+		
+	def createCivilian(self):
+		if self.separability == "hard":
+			return agent.Civilian(self.system, self.generateName(), 0.6, "resource", 6, 2)
+		elif self.separability == "medium":
+			return agent.Civilian(self.system, self.generateName(), 0.6, "resource", 6, 1)
+		else:
+			return agent.Civilia(self.system, self.generateName(), 0.6, "resource", 6, 1)
+		
+	def createNSACivilian(self):
+		if self.separability == "hard":
+			return NSAagent.NSACivilian(self.system, self.generateName(), self.nsa_elements, 30, 0.6, "resource", 6, 2)
+		elif self.separability == "medium":
+			return NSAagent.NSACivilian(self.system, self.generateName(), self.nsa_elements, 30, 0.6, "resource", 6, 1)
+		else:
+			return NSAagent.NSACivilian(self.system, self.generateName(), self.nsa_elements, 30, 0.6, "resource", 6, 1)
+		
+	def createGangster(self):
+		if self.separability == "hard":
+			return agent.Gangster(self.system, self.generateName(), 0.3, "resource", 3, 2)
+		elif self.separability == "medium":
+			return agent.Gangster(self.system, self.generateName(), 0.3, "resource", 3, 1)
+		else:
+			return agent.Gangster(self.system, self.generateName(), 0.0)
